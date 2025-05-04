e74822a7c0cdbcf709141ff16f6c5b093d892076
PreciousIfeaka <preciousifeaka@gmail.com>
2025-05-04 05:10:15 +0100

    fix: fixed theme toggle position

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -21,18 +21,15 @@ export const ThemeToggle = () => {
   }
 
   return (
-    <div className='relative'>
-      <button
-        onClick={() => {
-          const newTheme = theme === 'dark' ? 'light' : 'dark';
-          setTheme(newTheme);
-          localStorage.setItem('theme', newTheme);
-        }}
-        className='absolute top-4 right-4 p-2 bg-transparent text-gray-800 dark:text-gray-200 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700 transition'
-      >
-        {theme === 'dark' ? <Sun className="h-8 w-8" /> : <Moon className="h-8 w-8" />}
-        <span className="sr-only">Toggle theme</span>
-      </button>
-    </div>
+    <button
+      onClick={() => {
+        const newTheme = theme === 'dark' ? 'light' : 'dark';
+        setTheme(newTheme);
+        localStorage.setItem('theme', newTheme);
+      }}
+    >
+      {theme === 'dark' ? <Sun className="h-8 w-8" /> : <Moon className="h-8 w-8" />}
+      <span className="sr-only">Toggle theme</span>
+    </button>
   );
 };
